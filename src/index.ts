74c2ae3cf765e558bcbd9d3632b355818a700096
Philipp Borutta <phb@koempf24.de>
2022-05-05 16:46:17 +0200

    new test commit because of error message

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,57 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+const container: HTMLElement | any = document.getElementById("app")
+const numberOfPokemon: number = 151
+let dataLoaded = false
 
-import { helloWorld, Beispiel } from "./myModule";
-import { alertMe } from "./myOtherModule";
-
-console.log(helloWorld);
-customElements.define("my-beispiel", Beispiel);
-
-alertMe();
-
-const myInputValue = document.querySelector<HTMLInputElement>("#myInput");
-
-const myInputValueAlternate = document.querySelector(
-  "#myInput"
-) as HTMLInputElement;
-
-document
-  .querySelector<HTMLInputElement>("#myInput")
-  ?.addEventListener("focus", doSmth);
-
-function doSmth(e: UIEvent) {
-  const val = e.target as HTMLInputElement;
-  console.log(e, val.value);
+interface pokemonData {
+    name: string;
+    image: string;
 }
+
+ let pokemonObjects:pokemonData[] = []
+
+const fetchData = async (): Promise<void> => {
+    for (let i = 1; i <= numberOfPokemon; i++) {
+        pokemonObjects.push(await getPokemon(i));
+    }
+
+    dataLoaded = true
+    showAllPokemon()
+}
+
+async function getPokemon(id: number): Promise<pokemonData> {
+    const newUrl: string = 'https://pokeapi.co/api/v2/pokemon/'.concat(id.toString())
+    const data: Response = await fetch(newUrl)
+    const pokemons: any = await data.json()
+
+        const transformedPokemon = {
+            name: pokemons.name, 
+            image: pokemons.sprites.front_default.toString(), 
+        }
+
+
+    return transformedPokemon
+
+}
+
+const showPokemon = (pokemon: pokemonData): void => {
+  let output: string = 
+      `<div class="card">
+            <img class="cardImage" src=${pokemon.image} alt=${pokemon.name} />
+            <h3 class="cardName">${pokemon.name}</h1>
+       </div>`
+  container.innerHTML += output
+}
+
+function showAllPokemon() {
+
+    if (dataLoaded) {
+        for (let i=0; i < pokemonObjects.length; i++) {
+
+            showPokemon(pokemonObjects[i])
+        }
+    }
+    else {
+        alert("Daten wurde noch nicht geladen.")
+    }
+} 
+fetchData() 
